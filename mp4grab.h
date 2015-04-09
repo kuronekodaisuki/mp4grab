47cc164a4dfc12ff067129c7a04aaba26ae567b5
Hiro Matsuoka <kuronekodaisuki@me.com>
2015-04-09 12:08:58 +0900

    move to movie.c

--- mp4grab.h
+++ mp4grab.h
@@ -42,11 +42,6 @@ typedef struct OutputStream {
     //float t, tincr, tincr2;
 } OutputStream;
 
-inline AVRational av_create_rational(int num, int den){
-	AVRational ret = { num, den };
-	return ret;
-}
-
 AVFrame *get_dummy_frame(OutputStream *stream);
 AVFrame *read_image_frame(OutputStream *stream, const char *filename);
 
