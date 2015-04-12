c908ee01b579ae770a8ffd41b14903fcc28f948c
Hiro Matsuoka <kuronekodaisuki@me.com>
2015-04-11 21:52:20 -0700

    add -L option

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gcc -o mp4grab movie.c mp4grab.c -lavcodec -lavformat -lavutil -lswscale -lswresample -lopencv_core -lopencv_imgproc -lopencv_highgui -lm
+gcc -o mp4grab movie.c mp4grab.c -L /usr/local/lib -lavcodec -lavformat -lavutil -lswscale -lswresample -lopencv_core -lopencv_imgproc -lopencv_highgui -lm
