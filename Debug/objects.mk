1a593d9789267d9b0d58da58b668110491bd559e
Hiro Matsuoka <kuronekodaisuki@me.com>
2015-04-22 08:29:54 -0700

    eclipse environment

---
+++ Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lavcodec -lavformat -lavutil -lavfilter -lswscale -lswresample -lopencv_core -lopencv_imgproc -lopencv_highgui
+
---
+++ Release/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lavcodec -lavfilter -lavformat -lavutil -lswscale -lswresample -lopencv_core -lopencv_imgproc -lopencv_highgui
+
